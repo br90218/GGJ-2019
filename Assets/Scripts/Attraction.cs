843a32739dd1b615bae0e52e85d6ff0e2457c201
sherryfan <sherryfan706@gmail.com>
2019-01-29 12:11:34 -0500

    fix attraction bug
    fix attraction bug

--- Assets/Scripts/Attraction.cs
+++ Assets/Scripts/Attraction.cs
@@ -100,7 +100,7 @@ public class Attraction : MonoBehaviour
     private void Onattract(string Objectname)
     {
         range.SetActive(true);
-        if (distance <= attractRange && !(movement.state == Movement.State.idle && anothermovement.state == Movement.State.idle))
+        if (distance <= attractRange && (anothermovement.state == Movement.State.flying || anothermovement.state == Movement.State.landing))
         {
             print(Input.GetKey(KeyCode.Backspace));
             linerender.enabled = true;
